89d058bb48a2c2540ebcfbc5f0d3a72f60487fc5
Pixelsz <pinkdarin@gmail.com>
2023-01-06 20:04:38 -0300

    finished data types

---
+++ tipos de dados/variaveis.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+var string = "string";
+
+// Declare uma variável contendo um número dentro de uma string
+var numerostring = "22";
+
+// Declare uma variável com a sua idade
+var idade = 22;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Victor ";
+var sobrenome = "Éckeli";
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var teste = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
