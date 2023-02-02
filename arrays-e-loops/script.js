74c65f7e91512272537e0ef71ed5a5b454d3bf60
Pixelsz <pinkdarin@gmail.com>
2023-02-02 17:28:12 -0300

    refactor: init code

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,84 @@
+// let jogos = ["Nier Automata", "God of War", "CS:GO"];
+switch (caseInSwitch) {
+  case 1:
+    answer = "alpha";
+    break;
+  case 2:
+    answer = "beta";
+    break;
+  case 3:
+    answer = "gamma";
+    break;
+  case 4:
+    answer = "delta";
+    break;
+}
+
+//-----------
+
+switch (val) {
+  case 1:
+    answer = "alpha";
+    break;
+  case 2:
+    answer = "beta";
+    break;
+  case 3:
+    answer = "gamma";
+    break;
+  case 4:
+    answer = "delta";
+    break;
+}
+
+//-------
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+//-------
+
+function phoneticLookup(val) {
+  let result = "";
+
+  let lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+
+  result = lookup[val];
+  return result;
+}
+
+phoneticLookup("charlie");
---
+++ everything-is-object/script.js
@@ -0,0 +1,9 @@
+// nomeie 3 propriedades ou métodos de strings
+let nome = "Victor Bacca Eckeli";
+let nomeMinusculo = nome.toLowerCase();
+let nomeMaiusculo = nome.toUpperCase();
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
